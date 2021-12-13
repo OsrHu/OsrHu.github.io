389d12b67ab46117b985f980ba3344d2bf342772
Osrhu <h1147246925@163.com>
2021-12-13 09:28:32 +0800

    Site updated: 2021-12-13 09:28:32

--- js/background.js
+++ js/background.js
@@ -1,7 +1,14 @@
-// //去除banner
-// var full_page = document.getElementsByClassName("full_page");
-// if (full_page.length != 0) {
-//   full_page[0].style.background = "transparent";
-// }
+// 去除banner
+var header = document.getElementsByClassName("page-header");
+if (header.length != 0) {
+    header[0].style.background = "transparent";
+}
 
 
+
+
+// 添加八毛卡通人物
+/*右下角添加卡通人物*/
+var bamao = '<i class="fas fa-arrow-up" style ="padding:8px" ></i><img style="max-width: 234%;transform: translate(-65px,-65px);" src="https://cdn.jsdelivr.net/gh/fudalijunyi/picture-bed/img/20200629182853.gif" title="回到顶部" data-ll-status="loaded" class="loaded">';
+/*添加到返回顶部按钮下*/
+document.getElementById("go-up").innerHTML = bamao
